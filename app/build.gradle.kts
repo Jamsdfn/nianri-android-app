c3a5870b8330e4b7c6821057edb8d92aaa8727ae
Jamsdfn <2419252842@qq.com>
2026-07-19 16:59:31 +0800

    build: release v0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.nianri.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
         buildConfigField("String", "APP_NAME", "\"念日\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
