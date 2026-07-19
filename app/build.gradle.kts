c62241b9bec6f3ca3285698c4ab226841f8d5691
Jamsdfn <2419252842@qq.com>
2026-07-19 19:21:43 +0800

    chore: release 0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.nianri.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
         buildConfigField("String", "APP_NAME", "\"念日\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
